d3203af91c66a48ba497b3f937c7d67a020dfc06
Kevin Picado <kvap224@gmail.com>
2023-01-09 09:59:51 -0600

    a

--- api/src/app.js
+++ api/src/app.js
@@ -6,15 +6,7 @@ const server = express();
 
 server.use(express.json());
 
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); //"http://localhost:3000"); // update to match the domain you will make the requfrom
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+server.use(morgan("dev"));
+server.use(cors({ origin: "*", methods: ["GET", "POST", "PUT", "DELETE"] }));
 
 export default server;

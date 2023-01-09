63faacf6c88bf52f3db70d609d7af96ec2233702
blackgg88 <saulgg770@gmail.com>
2023-01-09 11:17:07 -0300

    added morgan and config cors

--- api/src/app.js
+++ api/src/app.js
@@ -1,18 +1,21 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
 
 const server = express();
 
 server.use(express.json());
 
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); //"http://localhost:3000"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+server.use(morgan("dev"));
+
+server.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+//Falta routes
+//server.use("/", routes)
 
 export default server;
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,4 +2,7 @@ import { Router } from "express";
 
 const router = Router();
 
+//Falta los controllers
+//router.use("/tournaments", tournaments)
+
 export default router;

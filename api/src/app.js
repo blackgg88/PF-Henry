c3406d7f1a9681a287e7944d4f46fc2fd35bff5b
Kevin Picado <kvap224@gmail.com>
2023-01-09 08:21:46 -0600

    a

--- api/src/app.js
+++ api/src/app.js
@@ -7,7 +7,7 @@ const server = express();
 server.use(express.json());
 
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); //"http://localhost:3000"); // update to match the domain you will make the reque from
+  res.header("Access-Control-Allow-Origin", "*"); //"http://localhost:3000"); // update to match the domain you will make the requfrom
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",

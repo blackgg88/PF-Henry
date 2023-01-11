b89641168f59280839fb4860f765f3a3db99d675
_ArrascaetaFedericoIgnacio_ <_arrascaetaefdev@gmail.com_>
2023-01-11 10:53:14 -0300

    build(models): :construction: add relation user

--- api/src/data/models/Role.ts
+++ api/src/data/models/Role.ts
@@ -1,9 +1,15 @@
 import { prop, getModelForClass } from "@typegoose/typegoose";
 
+export enum RoleEnum {
+    ADMIN = 'admin',
+    USER = 'user',
+}
+
 export class Role {
-  @prop()
-  name: string;
+  @prop({enum:RoleEnum, default:RoleEnum.USER})
+  name: RoleEnum;
 }
 
 const RoleModel = getModelForClass(Role);
 export default RoleModel;
+

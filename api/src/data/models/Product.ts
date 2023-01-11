3b61e06cc9728bfea16e8042472d54f70f8b6d54
blackgg88 <saulgg770@gmail.com>
2023-01-11 13:51:14 -0300

    Revert "fix(Models): :white_check_mark: prop category of product model changed"
    This reverts commit e11bc8c559ec7f44d7db542ffd46c47e4f54bd60.

--- api/src/data/models/Product.ts
+++ api/src/data/models/Product.ts
@@ -1,4 +1,4 @@
-import { prop, getModelForClass, Ref } from "@typegoose/typegoose";
+import { prop, getModelForClass, Ref} from "@typegoose/typegoose";
 import { Category, CategoryEnum } from "./Category";
 
 export class Product {
@@ -14,7 +14,7 @@ export class Product {
   @prop({ type: String, required: true })
   brand: string;
 
-  @prop({ type: String })
+  @prop({ type: String, lowercase: true })
   image: string;
 
   @prop({ type: Number, default: 0 })
@@ -23,8 +23,8 @@ export class Product {
   @prop({ type: Number, default: 0 })
   rating: number;
 
-  @prop({ ref: Category, required: true })
-  categories: Ref<Category>[];
+  @prop({ref:Category, default:CategoryEnum, required:true})
+  categories: Ref<Category>;
 }
 
 const ProductModel = getModelForClass(Product);

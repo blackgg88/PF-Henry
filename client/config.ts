3ea1213960064b158728e2a1c7305a882190b23f
Kevin Picado <kvap224@gmail.com>
2023-01-13 09:54:57 -0600

    feat: :sparkles: add login with auth0
    login

---
+++ client/config.ts
@@ -0,0 +1,3 @@
+export const REACT_APP_AUTH0_DOMAIN = import.meta.env.VITE_AUTH0_DOMAIN;
+
+export const REACT_APP_AUTH0_CLIENT_ID = import.meta.env.VITE_AUTH0_CLIENT_ID;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import { Auth0Provider } from "@auth0/auth0-react";
+
+import { REACT_APP_AUTH0_DOMAIN, REACT_APP_AUTH0_CLIENT_ID } from "../config";
+
+const domain = REACT_APP_AUTH0_DOMAIN;
+const clientId = REACT_APP_AUTH0_CLIENT_ID;
+
+// const domain = "dev-ofbihxhi1ivqz16i.us.auth0.com";
+// const clientId = "2rlz5Jb0snWMf4Zj6DsHi4mHeDguREtP";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);

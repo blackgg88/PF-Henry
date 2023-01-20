7eec51feee87c29df4f519af5d879877009f4d7a
Yeraldinnesan <ysanabria92@hotmail.com>
2023-01-20 08:05:02 -0500

    feat(ShopView): :sparkles: Shop view

--- client/src/RenderCard/RenderCard.tsx
+++ client/src/RenderCard/RenderCard.tsx
@@ -1,43 +1,110 @@
-import React, { useEffect, useCallback } from "react";
-import "./RenderCard.css";
-import { useAppDispatch, useAppSelector } from '../Redux/hook'
+import React, { useEffect, useState } from "react";
+import iconStar from "../assets/images/icons/star.png";
+import { useAppDispatch, useAppSelector } from "../Redux/hook";
 import { ProductState } from "../Redux/slice/product.slice";
 import { getProduct } from "../Redux/slice/product.slice";
 import { productFetch } from "../Redux/slice/ProductController";
-
+import PaginationComp from "../components/Pagination";
+import { ChangeEvent } from "react";
+import QuickLookModal from "./QuickLookModal";
 
 const RenderCard: React.FC<{}> = () => {
+  const Allproduct: ProductState[] = useAppSelector(
+    (state) => state.productReducer
+  );
 
-  const Allproduct: ProductState[] = useAppSelector(state => state.productReducer)
-  
-  const dispatch = useAppDispatch()
- 
+  const dispatch = useAppDispatch();
+
   useEffect(() => {
-    if(!Allproduct.length) {
-      productFetch()
-      .then(res => {
-        dispatch(getProduct(res))
-      })
+    if (!Allproduct.length) {
+      productFetch().then((res) => {
+        dispatch(getProduct(res));
+      });
     }
-}, [Allproduct]);
+  }, [Allproduct]);
 
-  
+  //-----------------------> Helper Functions <----------------------
 
+  const priceFormat = (productPrice: number) => {
+    const formattedNumber = productPrice.toFixed(2);
+    return formattedNumber;
+  };
 
-  return <div className="container-RenderCard">
-    {
-      Allproduct?.map(product => {
-        return (
-          <div key={product.id} className="ProductCard">
-            <h3>{product.name}</h3>
-            <img src={product.images[0]} alt="image"/>
-            <p>{product.description.substring(0,200)}...</p> 
-            <h4>Price: {product.price}$</h4>
-          </div>
-        )
-      })
-    }
-  </div>;
+  //-----------------> PAGINATION <----------------------------------
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(6);
+
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  // const handlePageChange = (event: ChangeEvent<unknown>) => {
+  //   const pageNumber = Number((event.currentTarget as HTMLInputElement).value);
+  //   if (!isNaN(pageNumber)) {
+  //     setCurrentPage(pageNumber);
+  //   }
+  // };
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = Allproduct.slice(indexOfFirstItem, indexOfLastItem);
+
+  //-----------------------> QUICKLOOK MODAL  <-------------------------------
+
+  return (
+    <div className="container-RenderCard">
+      <div className="container-procucts">
+        {currentItems?.map((product) => {
+          return (
+            <div key={product.id} className="product-card">
+              <div className="content-product-name">
+                <h3 className="product-name">
+                  {product.name.substring(0, 51)}
+                </h3>
+              </div>
+              <div className="separator-top">.</div>
+              <div className="content-images">
+                <div
+                  className="image-card"
+                  style={{
+                    backgroundImage: `url(${product.images[0]})`,
+                    backgroundSize: "85%",
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat",
+                  }}
+                ></div>
+              </div>
+              {/* <img className="image-card" src={product.images[0]} alt="image" /> */}
+              {/* <p>{product.description.substring(0,200)}...</p>  */}
+
+              <div className="details">
+                <div className="populariry">
+                  <img src={iconStar} alt="icon" />
+                  <h4 className="rating">{product.rating}</h4>
+                </div>
+                <div>
+                  <QuickLookModal AllProducts={Allproduct} />
+                </div>
+                <div className="separator">.</div>
+                <div className="value">
+                  <h4 className="price">$ {priceFormat(product.price)}</h4>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="pagination">
+        <PaginationComp
+          itemsPerPage={itemsPerPage}
+          totalItems={Allproduct.length}
+          currentPage={currentPage}
+          handlePageChange={handlePageChange}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default RenderCard;

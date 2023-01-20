fb2ba0744713c06207162ebbb314fab4349680c5
_ArrascaetaFedericoIgnacio_ <_arrascaetaefdev@gmail.com_>
2023-01-19 22:10:01 -0300

    fix(dashboard-User): :bug: fix peticion purchase

--- client/src/components/dashboard_user/controller.ts
+++ client/src/components/dashboard_user/controller.ts
@@ -1,5 +1,5 @@
 export const controllerUser = (email: any)=>{
-    const response = fetch(`http://localhost:3001/checkout?email=${email}`)
+    const response = fetch(`http://localhost:3001/checkout/${email}`)
     .then(res => res.json())
     .then(res => {
         return res

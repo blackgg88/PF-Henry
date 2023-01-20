70c181c0322de9b7fa4ba0bd957fd73071a13295
HumbertoJr10 <humbertojrguerra@gmail.com>
2023-01-20 01:01:10 -0400

    style(Home): :art: Demo Version News Home Style

--- client/src/components/home_news_fake/NewsHome.tsx
+++ client/src/components/home_news_fake/NewsHome.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
-// IMAGE ="https://www.codedimension.com.ar/public/images/noticias/318-tecnologia-5g-que-son-las-redes-de-comunicacion-inalambrica.jpg"
+
 export const NewsHome = () => {
-const imagesNews=["1","2","3","4","5","6"]
+const imagesNews=[
+  "https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2020/08/26-noticias-tecnologia-comenzar-manana-informado-ultimo-2024411.jpg",
+  "https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2020/09/25-noticias-tecnologia-comenzar-manana-informado-ultimo-2058681.jpg",
+  "https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2022/09/29-noticias-tecnologia-comenzar-manana-informado-ultimo-2822215.jpg",
+  "https://ichef.bbci.co.uk/news/640/cpsprodpb/17DEE/production/_117347779_tumundo_opcion2copy.png",
+  "https://www.semana.com/resizer/g5TukiBvL6oC5_REJWhV5GPXgrc=/arc-anglerfish-arc2-prod-semana/public/FP6C4T7ROFE37PQRVGRNXPLRAU.jpeg",
+  "https://itpeers.com/wp-content/uploads/2020/10/3570310-copy-scaled.jpg"
+]
 
   return (
     <div className="news_fake_container">
@@ -9,7 +16,7 @@ const imagesNews=["1","2","3","4","5","6"]
             return(
                 <div className='news_fake_card'>
                     <img src={img} alt="news_image" />
-                </div>
+              </div>
             )
         })}
     </div>

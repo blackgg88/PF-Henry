d9accaff9bd7ef3faa08e2c8b8d48299aa035d56
_ArrascaetaFedericoIgnacio_ <_arrascaetaefdev@gmail.com_>
2023-01-20 22:34:08 -0300

    style: :art: news-styles

--- client/src/components/news/News.tsx
+++ client/src/components/news/News.tsx
@@ -25,6 +25,11 @@ interface NewsData {
 export const News = () => {
 
   const [news, setNews] = useState<NewsData[]>([]);
+  const[page, setPage] = useState<number>(1)
+  const [slicedNew, setSlicedNew] = useState<NewsData[]>([]);
+  
+
+
 
   useEffect(() => {
     const fetchNews = async () => {
@@ -33,53 +38,81 @@ export const News = () => {
       );
       const data = await response.json();
       setNews(data.articles);
+      const arr = data.articles.slice((page-1)*4,(page-1)*4+4)
+      setSlicedNew(arr)
     };
     fetchNews();
   }, []);
 
+  useEffect(() => {
+    const arr = news.slice((page-1)*4,(page-1)*4+4)
+    setSlicedNew(arr)
+    
+  }, [page])
+  
   console.log(news)
+  console.log(slicedNew)
+
+  const buttonHandler = () => {
+    if(page<news.length/4){
+      setPage(page+1)
+    }
+  }
+
+  const buttonHandler2 = () => {
+    if(page>1){
+      setPage(page-1)
+    }
+  }
 
   return (
     <div className="news_container">
+      
+    
       {
-        (news.length>4)&&(
+        (slicedNew.length>1)&&(
 
           <div className="news_container_firstLine">
         <div className="news_firstLine_cardRow">
           <div className="news_firstLine_firstCard">
-            <img src={news[0].urlToImage} alt="firstNews" />
+            <img src={slicedNew[0].urlToImage} alt="firstNews" />
             <div className="news_firstLine_Content">
-              <h1>{news[0].title.substring(0, 30) + '...'}</h1>
-              <p>{news[0].publishedAt.substring(0,10)}</p>
+              <h1>{slicedNew[0].title.substring(0, 30) + '...'}</h1>
+              <p>{slicedNew[0].publishedAt.substring(0,10)}</p>
             </div>
           </div>
           <div className="news_firstLine_secondCard">
-            <img src={news[1].urlToImage} alt="secondNews" />
+            <img src={slicedNew[1].urlToImage} alt="secondNews" />
             <div className="news_firstLine_Content">
-              <h1>{news[1].title.substring(0, 30) + '...'}</h1>
-              <p>{news[1].publishedAt.substring(0,10)}</p>
+              <h1>{slicedNew[1].title.substring(0, 30) + '...'}</h1>
+              <p>{slicedNew[1].publishedAt.substring(0,10)}</p>
             </div>
           </div>
         </div>
         <div className="news_firstLine_cardColumn">
           <div className="news_firstLine_thridCard">
-            <img src={news[2].urlToImage} alt="thirdNew" />
+            <img src={slicedNew[2].urlToImage} alt="thirdNew" />
             <div className="news_firstLine_Content">
-              <h1>{news[2].title.substring(0, 30) + '...'}</h1>
-              <p>{news[2].publishedAt.substring(0,10)}</p>
+              <h1>{slicedNew[2].title.substring(0, 30) + '...'}</h1>
+              <p>{slicedNew[2].publishedAt.substring(0,10)}</p>
             </div>
           </div>
           <div className="news_firstLine_fourCard">
-            <img src={news[3].urlToImage} alt="fourNew" />
+            <img src={slicedNew[3].urlToImage} alt="fourNew" />
             <div className="news_firstLine_Content">
-              <h1>{news[3].title.substring(0, 30) + '...'}</h1>
-              <p>{news[3].publishedAt.substring(0,10)}</p>
+              <h1>{slicedNew[3].title.substring(0, 30) + '...'}</h1>
+              <p>{slicedNew[3].publishedAt.substring(0,10)}</p>
             </div>
           </div>
         </div>
       </div>
         )
       }
+      <div className="news_paginated" >
+      <button className="news_button_paginated" onClick={ buttonHandler2}>Prev</button>
+      <p>{page}</p>
+      <button className="news_button_paginated"  onClick={ buttonHandler}>Next</button>
+      </div>
     </div>
   );
 };

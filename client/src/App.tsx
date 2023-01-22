0fc300a9739244f4f86bdcdd49ff8b80e78b4ed8
blackgg88 <saulgg770@gmail.com>
2023-01-22 12:14:00 -0300

    fix(Routes): :bug: fix routes admin

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <div>
-      {location.pathname !== '/admin' && <NavBar />}
+      {!location.pathname.includes('/admin') && <NavBar />}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/register' />

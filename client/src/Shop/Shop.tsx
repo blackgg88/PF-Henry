aec29021e39a68ff95d05f8c3f03ffdaa20ec63f
blackgg88 <saulgg770@gmail.com>
2023-01-23 08:03:36 -0300

    fix(filters): :bug: fix provisorio

--- client/src/Shop/Shop.tsx
+++ client/src/Shop/Shop.tsx
@@ -1,26 +1,25 @@
-import React from "react";
+import React from 'react';
 //import "./Shop.css";
 
-import FiltersPanel from "../Filters/FiltersPanel";
-import RenderCards from "../RenderCard/RenderCard";
-import { Link } from "react-router-dom";
-import Breadcrumb from "../components/BreadCrumb";
-import Card2 from "../components/CardBeta/CardBeta";
+import FiltersPanel from '../Filters/FiltersPanel';
+import { Link } from 'react-router-dom';
+import Breadcrumb from '../components/BreadCrumb';
+import Card2 from '../components/CardBeta/CardBeta';
 const Shop: React.FC<{}> = () => {
   return (
-    <div className="content-shop">
+    <div className='content-shop'>
       {/* <div className="content-navbar"> */}
-        {/* <Link to="/">
+      {/* <Link to="/">
           <button>Back</button>
         </Link> */}
-        {/* <div>content-navbar</div> */}
+      {/* <div>content-navbar</div> */}
       {/* </div> */}
-      <div className="breadcrumb">
-        <div className="base">
+      <div className='breadcrumb'>
+        <div className='base'>
           <Breadcrumb />
         </div>
       </div>
-      <div className="shop-contain">
+      <div className='shop-contain'>
         <FiltersPanel />
         <Card2 />
       </div>

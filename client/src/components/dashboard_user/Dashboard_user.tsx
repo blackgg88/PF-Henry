889b11f845d44c36dd650607a8de151456a5bf88
blackgg88 <saulgg770@gmail.com>
2023-01-23 15:49:32 -0300

    feat(dashboard-User): :sparkles: changes list of payments from user

--- client/src/components/dashboard_user/Dashboard_user.tsx
+++ client/src/components/dashboard_user/Dashboard_user.tsx
@@ -1,34 +1,33 @@
-import React, { useEffect, useState } from "react";
-import { useAuth0 } from "@auth0/auth0-react";
-import { controllerUser } from "./controller";
-import Navbar from "../navbar/Navbar";
-import logo from "../../assets/logo_smart_b.png";
-import verified_true from "../../assets/verified/verified_true.png";
-import verified_false from "../../assets/verified/verified_false.png";
+import React, { useEffect, useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { controllerUser } from './controller';
+import Navbar from '../navbar/Navbar';
+import logo from '../../assets/logo_smart_b.png';
+import verified_true from '../../assets/verified/verified_true.png';
+import verified_false from '../../assets/verified/verified_false.png';
 
 // creame una interface para este estado const { purchase, setPurchase} = useState ([]) ;
-interface items {
+interface Items {
   title: string;
   id: string;
   unit_price: number;
-  description: string;
   quantity: number;
   picture_url: string;
+  category_id: string;
 }
 
-interface preference {
-  date_create: string;
-  items: items[];
-}
-
-interface Product {
-  title: string;
-  quantity: number;
+interface Payments {
+  date_created: string;
+  id: string;
+  items: Items[];
+  status: string;
+  status_detail: string;
+  total_paid_amount: number;
 }
 
 export const Dashboard_user = () => {
   const { user, isAuthenticated } = useAuth0();
-  const [purchase, setPurchase] = useState<preference[]>([]);
+  const [purchase, setPurchase] = useState<Payments[]>([]);
   //purchase [{}, {}]
 
   //const email = 'Humberto@gmail.com';
@@ -40,13 +39,7 @@ export const Dashboard_user = () => {
     const handleGetItems = async () => {
       const response = await controllerUser(email);
       console.log(response); //array de dos elementos
-      const mapeado = response.map((dato: any) => {
-        return {
-          date_created: dato.date_created,
-          items: dato.items,
-        };
-      });
-      setPurchase(mapeado);
+      setPurchase(response);
     };
     if (isAuthenticated) {
       handleGetItems();
@@ -56,20 +49,39 @@ export const Dashboard_user = () => {
     console.log(verified);
   }
 
+  const handleProducts = (items: Items[]) => {
+    const itemsNames = items.map(
+      (item) => item.title.slice(0, 30) + ' X ' + item.quantity,
+    );
+
+    return itemsNames.join('\n');
+  };
+
+  const handleFormatedDate = (date_created: string) => {
+    const dateString = date_created;
+    const date = new Date(dateString);
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    const formatedDate = `${day}-${month}-${year}`;
+
+    return formatedDate;
+  };
+
   return (
-    <div className="all">
-      <div className="dash_profileContainer">
-        <div className="dash_profile_ImgSide">
-          <img src={user?.picture} alt="picture-profile" />
+    <div className='all'>
+      <div className='dash_profileContainer'>
+        <div className='dash_profile_ImgSide'>
+          <img src={user?.picture} alt='picture-profile' />
         </div>
-        <div className="dash_profile_InfoSide">
+        <div className='dash_profile_InfoSide'>
           <h2>{user?.name}</h2>
           <p>{email}</p>
           <p>
             {verified ? (
-              <img src={verified_true} alt="verified_true" />
+              <img src={verified_true} alt='verified_true' />
             ) : (
-              <img src={verified_false} alt="verified_false" />
+              <img src={verified_false} alt='verified_false' />
             )}
           </p>
           {
@@ -84,58 +96,67 @@ export const Dashboard_user = () => {
         </div>
       </div>
 
-      <div className="dash_infouser_container">
-        <div className="dash_infouser_title">
+      <div className='dash_infouser_container'>
+        <div className='dash_infouser_title'>
           <img
-            className="dash_infouser_imageMenu"
-            src="https://icon-library.com/images/profile-png-icon/profile-png-icon-24.jpg"
-            alt="profileInfo"
+            className='dash_infouser_imageMenu'
+            src='https://icon-library.com/images/profile-png-icon/profile-png-icon-24.jpg'
+            alt='profileInfo'
           />
           <h2>My Information</h2>
         </div>
         <p>Manage your personal data</p>
       </div>
 
-      <div className="dash_purchaseDiv">
-        <div className="dash_purchaseTitleContainer">
+      <div className='dash_purchaseDiv'>
+        <div className='dash_purchaseTitleContainer'>
           <img
-            className="dash_purchase_iconMenu"
-            src="https://icon-library.com/images/purchase-icon-png/purchase-icon-png-8.jpg"
-            alt="cartPurchase"
+            className='dash_purchase_iconMenu'
+            src='https://icon-library.com/images/purchase-icon-png/purchase-icon-png-8.jpg'
+            alt='cartPurchase'
           />
           <h2>My shopping</h2>
         </div>
 
         {purchase.length ? (
-          purchase.map((product, index) => (
-            <div
-              key={product.date_create + "_" + index}
-              className="dash_Allpurchase_container"
-            >
-              {product.items.map((sell, index) => {
-                return (
-                  <div
-                    className="dash_onePurchase"
-                    key={sell.title + "_" + index}
-                  >
-                    <div className="dash_onePurchase_imageSide">
-                      <img
-                        className="imagePurchase"
-                        src={sell.picture_url}
-                        alt="imgPurchase"
-                      />
+          <div className='dash_Allpurchase_container'>
+            {purchase.map((payment) => {
+              return (
+                <div className='dash_onePurchase' key={payment.id}>
+                  <div className='dash_onePurchase_imageSide'>
+                    <img
+                      className='imagePurchase'
+                      src={payment.items[0].picture_url}
+                      alt='imgPurchase'
+                    />
+                  </div>
+                  <div className='dash_onePurchase_infoSide'>
+                    <h3>Items:</h3>
+                    <div className='dash_onePurchase_infoItems'>
+                      <ol>
+                        {payment.items.map((item) => (
+                          <li>{`${item.title.slice(0, 45)}   \nQuantity: ${
+                            item.quantity
+                          }`}</li>
+                        ))}
+                      </ol>
                     </div>
-                    <div className="dash_onePurchase_infoSide">
-                      <h3>{sell.title}</h3>
-                      <p>Quantity: {sell.quantity}</p>
-                      <p>Total: ${sell.unit_price * sell.quantity}</p>
-                      <p>{product.date_create}</p>
+                    <div className='dash_onePurchase_PaymentInfo'>
+                      <p>
+                        Total: <span>${payment.total_paid_amount.toFixed(2)}</span>
+                      </p>
+                      <p>
+                        <span>{handleFormatedDate(payment.date_created)}</span>
+                      </p>
+                      <p>
+                        Status: <span> {payment.status}</span>
+                      </p>
                     </div>
                   </div>
-                );
-              })}
-            </div>
-          ))
+                </div>
+              );
+            })}
+          </div>
         ) : (
           <div>
             <p>You have not made any purchases yet</p>

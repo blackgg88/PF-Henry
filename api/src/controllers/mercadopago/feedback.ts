3906b78397f1418040ad6266e1f9d2f9b4f99658
Kevin Picado <kvap224@gmail.com>
2023-01-26 12:06:50 -0600

    feat(NodeMailer): :sparkles: create template for the email

--- api/src/controllers/mercadopago/feedback.ts
+++ api/src/controllers/mercadopago/feedback.ts
@@ -1,16 +1,18 @@
-import axios from 'axios';
-import { Request, Response } from 'express';
+import axios from "axios";
+import { Request, Response } from "express";
+import { sendMailCreate } from "../../config/mailer";
 
-import mercadopago from 'mercadopago';
-import { ACCESS_TOKEN } from '../../../config';
+import mercadopago from "mercadopago";
+import { ACCESS_TOKEN } from "../../../config";
 
 mercadopago.configure({ access_token: ACCESS_TOKEN! });
 
 export const feedback = async (req: Request, res: Response) => {
-  console.log('funciona uwu');
+  console.log("funciona uwu");
   console.log(req.query); // data.id type
 
   console.log(req.body); //id, client_id
+  sendMailCreate("Pepe", "saulp496@gmail.com");
 
-  res.status(200).json({ message: 'hola' });
+  res.status(200).json({ message: "hola" });
 };

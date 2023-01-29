ec7bb294699b31e62453f330d5adc697dcf79472
HumbertoJr10 <humbertojrguerra@gmail.com>
2023-01-28 20:24:43 -0400

    fix(forum): :bug: Fix like route, added styles for filter panel, minor fix

--- client/helpers/foro/getLikes.ts
+++ client/helpers/foro/getLikes.ts
@@ -2,7 +2,7 @@ import { API_URL } from "../../config";
 
 export const getLikes = async (body: any, path: string) => {
   console.log(body)
-  return await fetch(`${API_URL}/${path}/like`, {
+  return await fetch(`${API_URL}/${path}/likes`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(body),
--- client/src/components/foro/components/FIlter_panel.tsx
+++ client/src/components/foro/components/FIlter_panel.tsx
@@ -1,3 +1,7 @@
+import findIcon from '../../../assets/foro/findIcon.svg';
+import refreshIcon from '../../../assets/foro/refresh.svg';
+import logoSmartW from '../../../assets/logo_smart_w.png'
+
 interface filterPanel {
   searchInput: string;
   onChangeSearch: any;
@@ -18,18 +22,29 @@ export default function FilterPanel({
   resetFilter,
 }: filterPanel) {
   return (
-    <div className='foro_menuTags_container'>
-      <form onSubmit={handleFilterByTitle}>
-        <input
-          name='title'
-          value={searchInput}
-          onChange={onChangeSearch}
-          type='text'
-          placeholder='Search'
-        />
-        <button>Search</button>
-      </form>
-      <button onClick={resetFilter}>Reset</button>
+    <div className='foro_panelContainer'>
+      <div>
+        <form className="foro_panel_SearchContainer" onSubmit={handleFilterByTitle}>
+          <input
+            name='title'
+            value={searchInput}
+            onChange={onChangeSearch}
+            type='text'
+            placeholder='Search'
+          />
+          <button>
+            <img className='foro_panel_findIcon' src={findIcon} alt="find" />
+          </button>
+        </form>
+        
+        <div onClick={resetFilter} className='foro_panel_refreshContainer'>
+          <img src={refreshIcon} alt="refresh" />
+          <p>Actualizar</p>
+        </div>
+      </div>
+      <div className='foro_panel_logoContainer'>
+        <img src={logoSmartW} alt="logoSmartNEST" />
+      </div>
     </div>
   );
 }

9f148332543ff2b649564f8db03342b2a940367f
HumbertoJr10 <humbertojrguerra@gmail.com>
2023-01-28 22:00:57 -0400

    style(forum): :art: Panel Filter styles beta

--- client/src/components/foro/components/FIlter_panel.tsx
+++ client/src/components/foro/components/FIlter_panel.tsx
@@ -1,6 +1,16 @@
 import findIcon from '../../../assets/foro/findIcon.svg';
 import refreshIcon from '../../../assets/foro/refresh.svg';
-import logoSmartW from '../../../assets/logo_smart_w.png'
+import logoSmartW from '../../../assets/logo_smart_w.png';
+import profileIconW from '../../../assets/foro/profile-white.svg';
+import homeIcon from '../../../assets/foro/homeIconW.svg';
+import connectivityIconW from '../../../assets/foro/connectivityIconW.svg';
+import entertainmentW from '../../../assets/foro/EntertainmentW.svg';
+import energyW from '../../../assets/foro/energyW.svg';
+import securityW from '../../../assets/foro/securityW.svg'
+import healthW from '../../../assets/foro/health.svg'
+
+
+
 
 interface filterPanel {
   searchInput: string;
@@ -41,6 +51,55 @@ export default function FilterPanel({
           <img src={refreshIcon} alt="refresh" />
           <p>Actualizar</p>
         </div>
+
+
+        {
+          
+          
+          <div className='foro_panel_MenuContainer'>
+            <div className='foro_panel_Menu_HomeContainer'>
+              <img src={healthW} alt="profile_Icon" />
+              <p>Lifestyle and Health</p>
+            </div>
+            <div className='foro_panel_Menu_HomeContainer'>
+              <img src={connectivityIconW} alt="connectivity" />
+              <p>Connectivity and Control</p>
+            </div>
+            <div className='foro_panel_Menu_HomeContainer'>
+              <img src={entertainmentW} alt="entertainment" />
+              <p>Home Entertainment</p>
+            </div>
+            <div className='foro_panel_Menu_HomeContainer'>
+              <img src={energyW} alt="energy" />
+              <p>Energy Management</p>
+            </div>
+            <div className='foro_panel_Menu_HomeContainer'>
+              <img src={securityW} alt="security" />
+              <p>Safety and Security</p>
+            </div>
+          </div>
+          
+          
+          
+        }
+        {
+          /* 
+          
+          <div className='foro_panel_tagsContainer'>
+            <fieldset>
+              <legend>Tags</legend>
+              <p>Connectivity and Control</p>
+              <p>Home Entertainment</p>
+              <p>Energy Management</p>
+              <p>Safety and Security</p>
+              <p>Comfort and Ease</p>
+              <p>Lifestyle and Health</p>
+            </fieldset>
+          </div>
+          
+          */
+        }
+
       </div>
       <div className='foro_panel_logoContainer'>
         <img src={logoSmartW} alt="logoSmartNEST" />

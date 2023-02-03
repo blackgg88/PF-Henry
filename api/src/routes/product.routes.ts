6526544f47e297513185f4360b343faa3d7afd37
DiegoKernel <gonzalezdiego.contact@gmail.com>
2023-02-03 12:30:03 -0300

    feat(Routes): :construction_worker: add quantity from products of categories

--- api/src/routes/product.routes.ts
+++ api/src/routes/product.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { getAllProduct } from '../controllers/product/getAllProduct'
+import { getQuantityCategories } from "../controllers/product/getQuantityCategories";
 import { getProductById } from '../controllers/product/getProductById'
 import { postProduct } from '../controllers/product/postProduct'
 import { deleteProduct } from '../controllers/product/deleteProduct'
@@ -11,6 +12,9 @@ const products = Router()
 
 //find products// devuelve lista de todos los productos
 products.get('/', getAllProduct)
+
+//find quantity from products // devuelve lista de la cantidad de productos por categorias
+products.get('/quantity/', getQuantityCategories)
 
 //find by ID // devuelve el products buscado por ID
 products.get('/:id', getProductById)

607371db6f36c6d150790aec4be4c1d71cb577e8
blackgg88 <saulgg770@gmail.com>
2023-02-07 14:30:55 -0300

    fix profile

--- client/helpers/user/getUserByEmail.ts
+++ client/helpers/user/getUserByEmail.ts
@@ -1,9 +1,9 @@
-import { API_URL } from "../../config";
+import { API_URL } from '../../config';
 
-export const getUserByEmail = async (email: string)=> {
-    const user =  fetch(`${API_URL}/users/${email}`)
-    .then( res => res.json())
-    .then(res => res)
+export const getUserByEmail = async (email: string) => {
+  const user = fetch(`${API_URL}/users/email/${email}`)
+    .then((res) => res.json())
+    .then((res) => res);
 
-    return await user
-}
+  return await user;
+};
--- client/helpers/user/getUserByID.ts
+++ client/helpers/user/getUserByID.ts
@@ -1,10 +1,10 @@
-import { API_URL } from "../../config";
+import { API_URL } from '../../config';
 
-export const UserByID = async (id: string)=> {
-    console.log(id)
-    const user =  fetch(`${API_URL}/users/id/${id}`)
-    .then( res => res.json())
-    .then(res => res)
+export const UserByID = async (id: string) => {
+  console.log(id);
+  const user = fetch(`${API_URL}/users/${id}`)
+    .then((res) => res.json())
+    .then((res) => res);
 
-    return await user
-}
+  return await user;
+};

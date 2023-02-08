f5177b29c71f8cb15d52fc8e86d53c6499351bd9
DiegoKernel <gonzalezdiego.contact@gmail.com>
2023-02-08 20:31:52 -0300

    style(Styles): :lipstick: add styles from news, and responsive

--- client/src/components/news/News.tsx
+++ client/src/components/news/News.tsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
 import { NEWS_API_KEY } from "../../../config";
 
+// import data from "./object.json";
+
 const api_key: string = NEWS_API_KEY;
 
 interface NewsData {
   title: string;
-  media: string;
-  url: string;
   published_date: string;
+  link: string;
+  media: string;
 }
 
 export const News = () => {
@@ -16,31 +18,29 @@ export const News = () => {
   const [slicedNew, setSlicedNew] = useState<NewsData[]>([]);
 
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'x-api-key': api_key
-    }
+      "x-api-key": api_key,
+    },
   };
-  console.log(slicedNew);
-  
+
   const params = new URLSearchParams({
-    q: 'smart home',
+    q: "smart home",
   });
-  
+
   const url = `https://api.newscatcherapi.com/v2/search?${params}`;
   useEffect(() => {
     const fetchNews = async () => {
       const response = await fetch(url,options);
       const data = await response.json();
-      console.log(data.articles);
+
       setNews(data.articles);
       const arr = data.articles.slice((page - 1) * 4, (page - 1) * 4 + 4);
       setSlicedNew(arr);
-    }
+    };
     fetchNews();
-    
   }, []);
-   
+
   useEffect(() => {
     const arr = news.slice((page - 1) * 4, (page - 1) * 4 + 4);
     setSlicedNew(arr);
@@ -60,62 +60,34 @@ export const News = () => {
 
   return (
     <div className="news_container">
-      {slicedNew.length > 0 ? (
-        <div className="news_container_firstLine">
-          {/* contenedor de las tarjetas(a) */}
-          <div className="news_firstLine_cardRow">
-            <a
-              className="news_firstLine_firstCard"
-              href={slicedNew[0].url}
-              target="_blank"
-            >
-              <img src={slicedNew[0].media} alt="firstNews" />
-              <div className="news_firstLine_Content">
-                <h1>{slicedNew[0].title.substring(0, 27) + "..."}</h1>
-                <p>{slicedNew[0].published_date.substring(0, 10)}</p>
-              </div>
-            </a>
+      {slicedNew?.length > 0 ? (
+        <div className="news_firstLine">
 
-            <a
-              className="news_firstLine_secondCard"
-              href={slicedNew[1].url}
-              target="_blank"
-            >
-              <img src={slicedNew[1].media} alt="secondNews" />
-              <div className="news_firstLine_Content">
-                <h1>{slicedNew[1].title.substring(0, 27) + "..."}</h1>
-                <p>{slicedNew[1].published_date.substring(0, 10)}</p>
-              </div>
-            </a>
-          </div>
-          <div className="news_firstLine_cardColumn">
-            <a
-              className="news_firstLine_thridCard"
-              href={slicedNew[2].url}
-              target="_blank"
-            >
-              <img src={slicedNew[2].media} alt="thirdNew" />
-              <div className="news_firstLine_Content">
-                <h1>{slicedNew[2].title.substring(0, 30) + "..."}</h1>
-                <p>{slicedNew[2].published_date.substring(0, 10)}</p>
-              </div>
-            </a>
-            <a
-              className="news_firstLine_fourCard"
-              href={slicedNew[3].url}
-              target="_blank"
-            >
-              <img src={slicedNew[3].media} alt="fourNew" />
-              <div className="news_firstLine_Content">
-                <h1>{slicedNew[3].title.substring(0, 30) + "..."}</h1>
-                <p>{slicedNew[3].published_date.substring(0, 10)}</p>
-              </div>
-            </a>
-          </div>
+          {slicedNew.map(( news ) => (
+            <div className="news_cardRow">
+              <a
+                className="news_card"
+                href={news.link}
+                target="_blank"
+              >
+                <img src={news.media} alt="firstNews" />
+                <div className="news_content">
+                  <h1>{news.title.substring(0, 27) + "..."}</h1>
+                  <p>{news.published_date.substring(0, 10)}</p>
+                </div>
+              </a>
+
+            </div>
+          ))}
+
         </div>
       ) : (
         <div className="news_container_firstLine">
-          <img className="news_container_loader" src="https://usagif.com/wp-content/uploads/loading-25.gif" alt="loader" />
+          <img
+            className="news_loader"
+            src="https://feccoo-madrid.org/f4404720ece11355df318a0acf525cb1000063.gif"
+            alt="loading"
+          />
         </div>
       )}
       <div className="news_paginated">
@@ -132,3 +104,5 @@ export const News = () => {
 };
 
 export default News;
+
+

13cc1c06b7d19a0c471382a3d3ca0eebb64ad258
DiegoKernel <gonzalezdiego.contact@gmail.com>
2023-02-07 23:28:06 -0300

    style(Styles): :lipstick: deleted color blue in sidebar

--- client/src/components/dashboard_admin/AppAdmin.tsx
+++ client/src/components/dashboard_admin/AppAdmin.tsx
@@ -22,7 +22,7 @@ import { MyAppBar } from './MyAppBar';
 import dataProvider from './dataProvider';
 
 const MyLayout = (props: any) => (
-  <Layout {...props} appBar={MyAppBar} style={{ backgroundColor: ` #2b4076` }} />
+  <Layout {...props} appBar={MyAppBar}  />
 );
 // console.log(dataProvider);
 

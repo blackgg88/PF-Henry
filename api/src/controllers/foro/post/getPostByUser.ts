5dcc65119f0add32fde5b191c6fc7ea409082b17
blackgg88 <saulgg770@gmail.com>
2023-04-15 23:44:48 -0300

    fix

--- api/src/controllers/foro/post/getPostByUser.ts
+++ api/src/controllers/foro/post/getPostByUser.ts
@@ -8,37 +8,34 @@ const PostModel = getModelForClass(Post);
 const UserModel = getModelForClass(User);
 
 export const getPostByUser = async (req: Request, res: Response) => {
-    
-    
-    try {
-        const {email} = req.params
+  try {
+    const { email } = req.params;
 
-        const Allposts = await PostModel.find()
-        .populate("author")
-        .populate({
-            path: "comments",
-            select: "content author likes deleted created",
-            populate: {
-                path: "author",
-            }
-        })
-        .select('-__v');
+    const Allposts = await PostModel.find()
+      .populate('author')
+      .populate({
+        path: 'comments',
+        select: 'content author likes deleted created',
+        populate: {
+          path: 'author',
+        },
+      })
+      .select('-__v');
 
-        const user = await UserModel.findOne({ email: email })
-      
+    const user = await UserModel.findOne({ email: email });
 
-        if (user) {
-            res.json(Allposts.filter(e=> {
-                if (!e.deleted && e.author._id==user._id.toString()) {
-                    return e
-                }
-            }).reverse())
-        } else {
-            return res.json({err: 'user no existe'})
-        }
-        
-
-    } catch (error) {
-        res.status(400).json({ message: 'Error getting post', error })
+    if (user) {
+      res.json(
+        Allposts.filter((e) => {
+          if (!e.deleted && e.author._id.toString() == user._id.toString()) {
+            return e;
+          }
+        }).reverse(),
+      );
+    } else {
+      return res.json({ err: 'user no existe' });
     }
-}
+  } catch (error) {
+    res.status(400).json({ message: 'Error getting post', error });
+  }
+};
